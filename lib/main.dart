ec00bf4a96e1bf72edcb2220420246cbf49a83c3
Dennis <doliveira@cgee.org.br>
2021-11-18 17:40:38 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage()
+
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+       children: <Widget>[
+         Card(
+           child: Text('Gráfico'),
+         ),
+         Card(
+           child: Text('Lista de Transações'),
+         ),
+       ],
+      ),
+    );
+  }
+}
