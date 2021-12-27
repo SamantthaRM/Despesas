b346c07c75aa86c119952f9c62c138ca1170499d
Dennis <doliveira@cgee.org.br>
2021-12-27 12:03:38 -0300

    images up2

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -29,7 +29,7 @@ class TransactionList  extends StatelessWidget {
           Container(
             height: 200,
             child: Image.asset(
-              'assets\images\waiting.png',
+              'assets/images/waiting.png',
             fit: BoxFit.cover,
             ),
           ),
