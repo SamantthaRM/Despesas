bf7420507eb027e02fcd884789e0ad70c38f837a
Dennis <doliveira@cgee.org.br>
2022-01-16 20:04:08 -0300

    Upp chart

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -21,8 +21,26 @@ class ChartBar  extends StatelessWidget {
           Container(
             height: 60,
             width: 10,
-            child: null,
-
+            child: Stack(
+             children: <Widget>[
+               Container(
+                 decoration: BoxDecoration(
+                   border: Border.all(
+                     color: Colors.blueGrey,
+                     width: 1.0,
+                   ),
+                   color: Color.fromRGBO(220, 220, 220, 1),
+                   borderRadius: BorderRadius.circular(5)
+                 ),
+               ),
+               FractionallySizedBox(
+                 heightFactor: percentage,
+                 child: Container(
+                   decoration: ,
+                 ),
+               ),
+             ],
+            ),
           ),
         SizedBox(height: 5),
         Text(label!,style:TextStyle(color: Colors.black)),
